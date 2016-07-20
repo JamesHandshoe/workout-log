a02bf7a6b2cc5a6087291aa206818b163e577689
James <jhandshoe@sbcglobal.net>
2016-07-20 15:36:55 -0400

    added tokens and authentication

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,18 +3,20 @@
 var router = require('express').Router();
 var sequelize = require('../db.js');
 var User = sequelize.import('../models/user');
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
 //this portion of the script takes the modeled 
 //user data and  and makes a virtual table
 router.post('/', function(req, res){
 
 		var username = req.body.user.username;
-		var pass = req.body.user.password; //TODO: hash this password
+		var pass = req.body.user.password; 
 
 		//the user model communicates with postgres to create a new user
 		User.create({
 			username: username,
-			passwordhash: '' //TODO: make it hashed
+			passwordhash: bcrypt.hashSync(pass, 10)
 		}).then(
 			function createSuccess(user){
 				res.json({
